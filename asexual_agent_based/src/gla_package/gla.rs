22bfc713987eb6438ca6fb0a44c3bfa61d7d19f2
spsalmon <psalmonsacha@gmail.com>
2024-04-16 11:12:09 +0200

    added the gla.rs comments to all the other folders

--- asexual_agent_based/src/gla_package/gla.rs
+++ asexual_agent_based/src/gla_package/gla.rs
@@ -1,8 +1,45 @@
+/// Calculate mortality based on the Gompertz model. 
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the mortality rate.
+/// * `aging_parameters` - An array containing the parameters for the Gompertz function:
+///     - `aging_parameters[0]` (a): The initial mortality rate (intercept).
+///     - `aging_parameters[1]` (b): The rate of increase in mortality rate with age (slope).
+///
+/// # Returns
+/// Returns the mortality rate at age `x`, calculated using the provided `a` and `b` parameters.
+///
+/// # Examples
+/// ```
+/// let age = 50.0;
+/// let parameters = [0.01, 0.02];
+/// let mortality_rate = _aging_gompertz(age, &parameters);
+/// assert_eq!(mortality_rate, 0.01 * (50.0 * 0.02).exp());
+/// ```
 pub fn _aging_gompertz(x: f64, aging_parameters: &[f64]) -> f64 {
     let (a, b) = (aging_parameters[0], aging_parameters[1]);
     a * (x * b).exp()
 }
 
+/// Calculate mortality based on the Gompertz-Makeham model.
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the mortality rate.
+/// * `aging_parameters` - An array containing the parameters for the Gompertz-Makeham function:
+///     - `aging_parameters[0]` (a): The initial mortality rate (intercept).
+///     - `aging_parameters[1]` (b): The rate of increase in mortality rate with age (slope).
+///     - `aging_parameters[2]` (c): Constant age-independent mortality factor.
+///
+/// # Returns
+/// Returns the mortality rate at age `x`, calculated using the provided `a`, `b`, and `c` parameters.
+///
+/// # Examples
+/// ```
+/// let age = 50.0;
+/// let parameters = [0.01, 0.02, 0.005];
+/// let mortality_rate = aging_gompertz_makeham(age, &parameters);
+/// assert_eq!(mortality_rate, 0.005 + 0.01 * (50.0 * 0.02).exp());
+/// ```
 pub fn aging_gompertz_makeham(x: f64, aging_parameters: &[f64]) -> f64 {
     let (a, b, c) = (
         aging_parameters[0],
@@ -12,6 +49,17 @@ pub fn aging_gompertz_makeham(x: f64, aging_parameters: &[f64]) -> f64 {
     c + a * (x * b).exp()
 }
 
+/// Calculate benefit of learning on mortality based on a modified logistic model.
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the learning benefit.
+/// * `learning_parameters` - An array containing the parameters for the learning curve:
+///     - `learning_parameters[0]` (lmax): The maximum learning benefit.
+///     - `learning_parameters[1]` (k): The inflection point of the curve.
+///     - `learning_parameters[2]` (n): The steepness of the curve.
+///
+/// # Returns
+/// Returns the benefit of learning on mortality at the input `x`, calculated using the provided `lmax`, `k`, and `n`.
 pub fn learning_function(x: f64, learning_parameters: &[f64]) -> f64 {
     let (lmax, k, n) = (
         learning_parameters[0],
@@ -21,11 +69,35 @@ pub fn learning_function(x: f64, learning_parameters: &[f64]) -> f64 {
     lmax * ((1_f64 / (1_f64 + (n * (x - k)).exp())) - 1_f64)
 }
 
+/// Calculate the benefit of growth on mortality.
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the growth benefit.
+/// * `growth_parameters` - An array containing the parameters for the growth curve:
+///     - `growth_parameters[0]` (gmax): The maximum growth benefit.
+///     - `growth_parameters[1]` (growth_rate): The steepness of the curve.
+///
+/// # Returns
+/// Returns the benefit of growth on mortality at the input `x`, calculated using the provided `gmax` and `growth_rate`.
 pub fn growth_function(x: f64, growth_parameters: &[f64]) -> f64 {
     let (gmax, growth_rate) = (growth_parameters[0], growth_parameters[1]);
     gmax * ((1_f64 / (1_f64 + x.powf(growth_rate))) - 1_f64)
 }
 
+/// Calculate mortality based on the GLA model.
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the mortality rate.
+/// * `aging_func` - The function to calculate the contribution of aging to mortality.
+/// * `learning_func` - The function to calculate the contribution of learning to mortality.
+/// * `growth_func` - The function to calculate the contribution of growth to mortality.
+/// * `aging_parameters` - An array containing the parameters for the aging function.
+/// * `learning_parameters` - An array containing the parameters for the learning function.
+/// * `growth_parameters` - An array containing the parameters for the growth function.
+/// * `minimum_mortality` - The minimum mortality rate.
+///
+/// # Returns
+/// Returns the mortality rate at age `x`, calculated using the provided functions and parameters. If the calculated mortality rate is less than the minimum mortality rate, the minimum mortality rate is returned instead
 pub fn gla_model<T>(
     x: f64,
     aging_func: T,
--- landscapes/fitness_landscape/src/gla_package/gla.rs
+++ landscapes/fitness_landscape/src/gla_package/gla.rs
@@ -1,11 +1,48 @@
 use ndarray::{Array, ArrayView1};
 use optimize::{Minimizer, NelderMeadBuilder};
 
+/// Calculate mortality based on the Gompertz model. 
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the mortality rate.
+/// * `aging_parameters` - An array containing the parameters for the Gompertz function:
+///     - `aging_parameters[0]` (a): The initial mortality rate (intercept).
+///     - `aging_parameters[1]` (b): The rate of increase in mortality rate with age (slope).
+///
+/// # Returns
+/// Returns the mortality rate at age `x`, calculated using the provided `a` and `b` parameters.
+///
+/// # Examples
+/// ```
+/// let age = 50.0;
+/// let parameters = [0.01, 0.02];
+/// let mortality_rate = _aging_gompertz(age, &parameters);
+/// assert_eq!(mortality_rate, 0.01 * (50.0 * 0.02).exp());
+/// ```
 pub fn _aging_gompertz(x: f64, aging_parameters: &[f64]) -> f64 {
     let (a, b) = (aging_parameters[0], aging_parameters[1]);
     a * (x * b).exp()
 }
 
+/// Calculate mortality based on the Gompertz-Makeham model.
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the mortality rate.
+/// * `aging_parameters` - An array containing the parameters for the Gompertz-Makeham function:
+///     - `aging_parameters[0]` (a): The initial mortality rate (intercept).
+///     - `aging_parameters[1]` (b): The rate of increase in mortality rate with age (slope).
+///     - `aging_parameters[2]` (c): Constant age-independent mortality factor.
+///
+/// # Returns
+/// Returns the mortality rate at age `x`, calculated using the provided `a`, `b`, and `c` parameters.
+///
+/// # Examples
+/// ```
+/// let age = 50.0;
+/// let parameters = [0.01, 0.02, 0.005];
+/// let mortality_rate = aging_gompertz_makeham(age, &parameters);
+/// assert_eq!(mortality_rate, 0.005 + 0.01 * (50.0 * 0.02).exp());
+/// ```
 pub fn aging_gompertz_makeham(x: f64, aging_parameters: &[f64]) -> f64 {
     let (a, b, c) = (
         aging_parameters[0],
@@ -15,6 +52,17 @@ pub fn aging_gompertz_makeham(x: f64, aging_parameters: &[f64]) -> f64 {
     c + a * (x * b).exp()
 }
 
+/// Calculate benefit of learning on mortality based on a modified logistic model.
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the learning benefit.
+/// * `learning_parameters` - An array containing the parameters for the learning curve:
+///     - `learning_parameters[0]` (lmax): The maximum learning benefit.
+///     - `learning_parameters[1]` (k): The inflection point of the curve.
+///     - `learning_parameters[2]` (n): The steepness of the curve.
+///
+/// # Returns
+/// Returns the benefit of learning on mortality at the input `x`, calculated using the provided `lmax`, `k`, and `n`.
 pub fn learning_function(x: f64, learning_parameters: &[f64]) -> f64 {
     let (lmax, k, n) = (
         learning_parameters[0],
@@ -24,11 +72,32 @@ pub fn learning_function(x: f64, learning_parameters: &[f64]) -> f64 {
     lmax * ((1_f64 / (1_f64 + (n * (x - k)).exp())) - 1_f64)
 }
 
+/// Calculate the benefit of growth on mortality.
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the growth benefit.
+/// * `growth_parameters` - An array containing the parameters for the growth curve:
+///     - `growth_parameters[0]` (gmax): The maximum growth benefit.
+///     - `growth_parameters[1]` (growth_rate): The steepness of the curve.
+///
+/// # Returns
+/// Returns the benefit of growth on mortality at the input `x`, calculated using the provided `gmax` and `growth_rate`.
 pub fn growth_function(x: f64, growth_parameters: &[f64]) -> f64 {
     let (gmax, growth_rate) = (growth_parameters[0], growth_parameters[1]);
     gmax * ((1_f64 / (1_f64 + x.powf(growth_rate))) - 1_f64)
 }
 
+/// Calculate the fertility based on the Brass polynomial model.
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the fertility rate.
+/// * `fertility_parameters` - An array containing the parameters for the Brass polynomial:
+///     - `fertility_parameters[0]` (c): Level parameter, proportional to the TFR.
+///     - `fertility_parameters[1]` (d): The age at which fertility begins.
+///     - `fertility_parameters[2]` (w): The width of the fertility window.
+///
+/// # Returns
+/// Returns the fertility rate at the input `x`, calculated using the provided `c`, `d`, and `w`.
 pub fn fertility_brass_polynomial(x: f64, fertility_parameters: &[f64]) -> f64 {
     let (c, d, w) = (
         fertility_parameters[0],
@@ -42,16 +111,34 @@ pub fn fertility_brass_polynomial(x: f64, fertility_parameters: &[f64]) -> f64 {
     }
 }
 
+/// Calculate the fertility based on a constant fertility model.
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the fertility rate.
+/// * `fertility_parameters` - An array containing the parameters for the constant fertility model:
+///     - `fertility_parameters[0]` (c): The constant fertility rate.
+///
+/// # Returns
+/// Returns the fertility rate at the input `x`, calculated using the provided `c`.
 pub fn constant_fertility(_x: f64, fertility_parameters: &[f64]) -> f64 {
     let c = fertility_parameters[0];
     c
 }
 
-pub fn linear_fertility(x: f64, fertility_parameters: &[f64]) -> f64 {
-    let (a, b) = (fertility_parameters[0], fertility_parameters[1]);
-    (a*x + b).max(0_f64)
-}
-
+/// Calculate mortality based on the GLA model.
+///
+/// # Arguments
+/// * `x` - The age at which to calculate the mortality rate.
+/// * `aging_func` - The function to calculate the contribution of aging to mortality.
+/// * `learning_func` - The function to calculate the contribution of learning to mortality.
+/// * `growth_func` - The function to calculate the contribution of growth to mortality.
+/// * `aging_parameters` - An array containing the parameters for the aging function.
+/// * `learning_parameters` - An array containing the parameters for the learning function.
+/// * `growth_parameters` - An array containing the parameters for the growth function.
+/// * `minimum_mortality` - The minimum mortality rate.
+///
+/// # Returns
+/// Returns the mortality rate at age `x`, calculated using the provided functions and parameters. If the calculated mortality rate is less than the minimum mortality rate, the minimum mortality rate is returned instead
 pub fn gla_model<T>(
     x: f64,
     aging_func: T,
@@ -78,6 +165,15 @@ where
     }
 }
 
+/// Find the age at which the fertility rate is maximized and the value of the maximum fertility rate for a given fertility function.
+///
+/// # Arguments
+/// * `fertility_function` - The fertility function to optimize.
+/// * `fertility_parameters` - An array containing the parameters for the fertility function.
+/// * `first_guess` - The initial guess for the age at which fertility is maximized.
+///
+/// # Returns
+/// Returns the maximum fertility rate of the input fertility function.
 pub fn find_maximum_fertility<T>(
     fertility_function: &T,
     fertility_parameters: &[f64],
